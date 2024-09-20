4f8790b11b50611fe1cea32fce9f2da3e638af7c
Russell Gordon <rgordon@lcs.on.ca>
2024-09-19 20:57:08 -0400

    Wrote function to find if a potential divisor is actually a divisor of a dividend.

--- RSANumbers/main.swift
+++ RSANumbers/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+func isDivisor(dividend: Int, divisor: Int) -> Bool {
+    // Add logic to determine whether provided divisor actually
+    // goes evenly into the provided dividend
+    // (e.g.: remainder is zero)
+ 
+    // Return the result...
+    if dividend % divisor == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Test
+print(isDivisor(dividend: 12, divisor: 4)) // true
+print(isDivisor(dividend: 13, divisor: 4)) // false
 
